f726f41c38f4e5bca76d21372edacb521b2fa50b
HakimAmraoui <hakim.amraoui@outlook.fr>
2020-10-22 21:02:34 +0300

    Jump + kunai
    The kunai turn around the player and point to the mouse. When the player left click, the ninja jump in that direction.

---
+++ Ninjump/Assets/PlayerMovements.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private Rigidbody2D rb;
+    // [SerializeField] private Camera cam;
+
+    // private Vector2 movement;
+    private Vector2 mousePos;
+    private Vector2 lookDir;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // When the player left click
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // By subtracting a vector by another we got a vector that points from one to the other  
+        lookDir = mousePos - rb.position;
+    }
+
+    void Jump()
+    {
+        //rb.AddForce(transform.forward * jumpForce, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(lookDir.x * jumpForce, lookDir.y * jumpForce);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+    }
+}
---
+++ Ninjump/Assets/Scripts/KunaiRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiRotation : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse Position in world game
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
