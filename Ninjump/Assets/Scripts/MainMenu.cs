d2ce56908b5121ed07065ac99e290667fdcffa16
HakimAmraoui <hakim.amraoui@outlook.fr>
2020-11-10 16:32:59 +0200

    How to play
    When you start to play, there is now a little guide which explains how to play.

--- Ninjump/Assets/Scripts/MainMenu.cs
+++ Ninjump/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    [SerializeField] private Image guide;
+    [SerializeField] private Button nextButton, endButton;
+    [SerializeField] private Text[] texts;
+
+
     public void PlayGame()
     {
         // We load the first level when we hit the play button
         SceneManager.LoadScene("Scene01");
     }
 
+    public void Option()
+    {
+    }
+
     public void QuitGame()
     {
         Application.Quit();
     }
+    
+    
+    /////////// How To Play ///////////
+
+    private int currentText = 0;
+    
+    public void StartGuide()
+    {
+        guide.gameObject.SetActive(true);
+        Continue();
+
+    }
+
+    private void Update()
+    {
+        if (currentText == texts.Length)
+        {
+            nextButton.gameObject.SetActive(false);
+            endButton.gameObject.SetActive(true);
+        } 
+    }
+
+
+    public void Continue()
+    {
+        if (currentText < texts.Length)
+        {
+            texts[currentText].gameObject.SetActive(true);
+            currentText++;
+        }
+        else Debug.Log("Fin");
+    }
+    
 }
