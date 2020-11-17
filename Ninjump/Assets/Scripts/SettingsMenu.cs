e730220be8d6bdc70fb462a3687776c055f1a326
HakimAmraoui <hakim.amraoui@outlook.fr>
2020-11-17 17:22:52 +0200

    Settings Menu
    Implemented the settings menu with the resolution, volume and full screen options.

---
+++ Ninjump/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer AudioMixer;
+
+    private Resolution[] _resolutions;
+    [SerializeField] private Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
+            options.Add(option);
+        }
+        
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioMixer.SetFloat("volume", volume);
+        // Debug.Log(volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
